7a3ce666e4324dc3f4163c46a698f73236065975
Ayana Bando <ayanaflax@yahoo.com>
2019-01-11 19:02:04 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,21 +1,21 @@
 // declare your functions here...
 
 function paragraphSelector(){
-  $("p");
+  return $("p");
 }
 
 function lastImageSelector(){
-  $('div img:last');
+ return $('div img:last');
 }
 
 function ninjaBabySelector(){
-  $('#baby-ninja');
+ return $('#baby-ninja');
 }
 
 function divSelector(){
-  $('div:.pics');
+ return $('div:.pics');
 }
 
 function firstListItem(){
-  $("ul :pic-list");
+  return $("ul :pic-list");
 }
